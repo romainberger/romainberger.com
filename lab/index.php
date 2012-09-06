5ab77138645cb13d06592472064bb9b67614f182
romainberger <signup@romainberger.com>
2012-09-07 01:06:30 +0200

    Initial commit

---
+++ lab/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Lab - Romain Berger</title>
+		<meta name="description" content="Romain Berger Lab" />
+		<link rel="stylesheet" type="text/css" href="../css/main.css" />
+	</head>
+	<body>
+		<div id="wrapper" class="wrapper_lab">
+			<div id="header"><a href="http://romainberger.com">
+	            <h1>Lab</h1>
+	            <h2>Stuff I make for fun</h2></a>
+	        </div>
+			<ul>
+				<li><a href="http://romainberger.com/lab/pixelbooth">PixelBooth<span> - A pixelated photobooth</span></a></li>
+				<li><a href="http://romainberger.com/lab/pure-css-stickaz-logo">Pure CSS Stickaz Logo<span> - No image, just CSS</span></a></li>
+				<li><a href="http://romainberger.com/lab/Paint/">HTML5 Paint<span> - Just to play with basic canvas stuff</span></a></li>
+			</ul>
+
+			<p><a href="http://romainberger.com">&lt;&lt; Back</a></p>
+		</div><!-- end wrapper -->
+	<script>
+        var _gaq=[['_setAccount','UA-25925169-1'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+	</body>
+</html>
