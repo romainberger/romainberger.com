14524accf4774188a862f2956cfdcb945e9b0ff1
romainberger <signup@romainberger.com>
2013-01-13 23:16:10 +0100

    Added some navigation stuffs

--- app/views.py
+++ app/views.py
@@ -7,16 +7,16 @@ def index():
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+	return render_template('about.html', page = 'about')
 
 @app.route('/web')
 def web():
-	return render_template('web.html')
+	return render_template('web.html', page = 'web')
 
 @app.route('/music')
 def music():
-	return render_template('music.html')
+	return render_template('music.html', page = 'music')
 
 @app.route('/lab')
 def lab():
-	return render_template('lab.html')
+	return render_template('lab.html', page = 'lab')
