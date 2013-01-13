dcbfaee1e42a2b38c3dcee2cb3bad37f065bf71c
romainberger <signup@romainberger.com>
2013-01-13 18:01:40 +0100

    More content, some css fixes

---
+++ app/static/js/main.js
@@ -0,0 +1,9 @@
+// really complicated piece of software
+!function($, window, document) {
+
+  $('#resume-trigger').click(function() {
+    $(this).slideUp(200)
+    $('#resume').slideDown(500)
+  })
+
+}(window.jQuery, window, document);
