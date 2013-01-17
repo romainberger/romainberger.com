ba83fc48eab9cf85f190ecce08def1184ab02631
romainberger <signup@romainberger.com>
2013-01-17 22:29:54 +0100

    Build previous commit

---
+++ build/static/js/main.js
@@ -0,0 +1,13 @@
+/*!
+ * romainberger.com
+ */
+
+// really complicated piece of software
+!function($, window, document) {
+
+  $('#resume-trigger').click(function() {
+    $(this).fadeOut(500)
+    $('#resume').fadeIn(500)
+  })
+
+}(window.jQuery, window, document);
