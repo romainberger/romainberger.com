d1304028cb16fee02874741a19f8536e8cc36c85
romainberger <romain@romainberger.com>
2013-07-11 01:30:26 +0200

    Started side nav

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,7 +2,7 @@
  * romainberger.com
  */
 
-!function() {
+!function($) {
 
   var trigger = document.querySelector('#resume-trigger')
     , resume  = document.querySelector('#resume')
@@ -15,4 +15,57 @@
     }, false)
   }
 
-}();
+  // Navigation panel for small screens
+  var navTrigger = $('.nav-trigger')
+    , sideNav = $('.side-nav')
+    , body = $('body')
+    , container = $('.main-content')
+    , opened = false
+
+  /**
+   * action: open if true else close
+   */
+  function animateNav(action) {
+    container.animate({
+      left: action ? '200px' : 0
+    }, 200)
+    sideNav.animate({
+      left: action ? 0 : '-200px'
+    }, 200)
+
+    if (action) {
+      body.addClass('no-scroll')
+      opened = true
+    }
+    else {
+      body.removeClass('no-scroll')
+      opened = false
+    }
+  }
+
+  function resizeSideNav() {
+    sideNav.css('height', $('body').height())
+  }
+
+  $(document).ready(function() {
+    resizeSideNav()
+
+    $(window).resize(resizeSideNav)
+
+    navTrigger.click(function() {
+      if (opened) {
+        animateNav(false)
+      }
+      else {
+        animateNav(true)
+      }
+    })
+
+    // @todo close on click when opened
+    // container.click(function() {
+    //   opened && animateNav(false)
+    // })
+
+  })
+
+}(window.jQuery);
