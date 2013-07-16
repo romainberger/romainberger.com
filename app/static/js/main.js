5d289f1b6a160a0e1a4f088c8b82e35ecc761001
romainberger <romain@romainberger.com>
2013-07-16 23:41:30 +0200

    Add jshint

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,11 +4,13 @@
 
 !function($) {
 
+  'use strict';
+
   var trigger = document.querySelector('#resume-trigger')
     , resume  = document.querySelector('#resume')
 
-  if (typeof trigger != 'undefined' && trigger != null
-      && typeof resume != 'undefined' && resume != null) {
+  if (typeof trigger != 'undefined' && trigger !== null &&
+      typeof resume != 'undefined' && resume !== null) {
     trigger.addEventListener('click', function() {
       this.style.display = 'none'
       resume.style.display = 'block'
