f0c7a8571fa14033bb1c59b9753036d81dcc0f2b
romainberger <romain@romainberger.com>
2013-08-06 00:09:05 +0200

    Fix prism plugin

--- _plugins/jekyll-prism-plugin.rb
+++ _plugins/jekyll-prism-plugin.rb
@@ -36,7 +36,7 @@ module Jekyll
 
       <<-HTML
 <div>
-  <pre data-line='#{@options["linenos"]}'><code class='language-#{@lang}'>#{code}</code></pre>
+  <pre class="language-#{@lang}" data-line="#{@options["linenos"]}"><code class="language-#{@lang}">#{code}</code></pre>
 </div>
       HTML
     end
